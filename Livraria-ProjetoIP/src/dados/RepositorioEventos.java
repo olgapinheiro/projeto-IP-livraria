f678e42098fad0602022b1b432ca220a4b2d16b0
vitorsilvaa <vsal@cin.ufpe.br>
2017-06-19 09:46:17 -0300

    Add files via upload

---
+++ Livraria-ProjetoIP/src/dados/RepositorioEventos.java
@@ -0,0 +1,12 @@
+package dados;
+
+import eventos.Evento;
+
+public interface RepositorioEventos {
+
+	void inserir(Evento evento);
+	void remover(String nome);
+	void atualizar(String nome);
+	Evento procurar(String nome);
+	boolean existe(String nome);
+}
