42046cca957a0cc4c09d6a25878538fd44d4d2b3
vitorsilvaa <vsal@cin.ufpe.br>
2017-06-20 10:48:05 -0300

    Add files via upload

---
+++ Livraria-ProjetoIP/src/classesNegocio/EventoTeatro.java
@@ -0,0 +1,34 @@
+package ClassesNegocio;
+import ClassesNegocio.Evento;
+import dados.RepositorioEventos;
+
+public class EventoTeatro extends Evento{
+	
+	private double precoIngresso;
+	private int quantidade;
+
+	public EventoTeatro(String nome, String data, String horario, double custo,double precoIngresso,int quantidade) {
+		super(nome, data, horario, custo);
+		this.precoIngresso = precoIngresso;
+		this.quantidade = quantidade;
+		
+	}
+
+	public double getPrecoIngresso() {
+		return precoIngresso;
+	}
+
+	public void setPrecoIngresso(double precoIngresso) {
+		this.precoIngresso = precoIngresso;
+	}
+
+	public double getQuantidade() {
+		return quantidade;
+	}
+
+	public void setQuantidade(int quantidade) {
+		this.quantidade = quantidade;
+	}
+	
+
+}
