a051e55ade43cdf101dfd814def62a5efcc662e7
marialuizaleal <mllb@cin.ufpe.br>
2017-06-21 14:38:46 -0300

    Add files via upload

---
+++ Livraria-ProjetoIP/src/classesNegocio/Livro.java
@@ -0,0 +1,50 @@
+package classesNegocio;
+
+public class Livro extends Produto {
+	private String genero;
+	private String numeroEdicao;
+	private int paginas;
+	private String autor;
+	private String editora;
+
+	public Livro(String codigo,double preco,String nome ,int estoque){
+		super(codigo, preco, nome , estoque);
+	}
+
+	public String getGenero() {
+		return this.genero;
+	}
+
+	public String getAutor() {
+		return this.autor;
+	}
+
+	public String getNumeroEdicao() {
+		return this.numeroEdicao;
+	}
+
+	public String getEditora() {
+		return this.editora;
+	}
+
+	
+	public void setGenero(String gen) {
+		this.genero = gen;
+	}
+
+	public void setNumeroEdicao(String numEd) {
+		this.numeroEdicao = numEd;
+	}
+
+	
+	public void setNumeroPaginas(int pag) {
+		this.paginas = pag;
+	}
+
+	public void setAutor(String autor) {
+		this.autor = autor;
+	}
+	public void setEditora(String ed) {
+		this.editora = ed;
+	}
+}
