d375dbf2a61c481869469c829c7028be4459b175
vitorsilvaa <vsal@cin.ufpe.br>
2017-06-21 07:36:37 -0300

    Add files via upload

---
+++ Livraria-ProjetoIP/src/dados/RepositorioEventoArrey.java
@@ -0,0 +1,63 @@
+package eventos;
+import dados.RepositorioEvento;
+import eventos.Evento;
+import eventos.EventoExistenteException;
+import eventos.EventoNaoExisteException;
+
+public class RepositorioEventoArrey implements RepositorioEvento{
+	
+	private Evento[] eventos;
+	private int indice;
+	
+	public RepositorioEventoArrey(int tamanho){
+		eventos = new Evento[tamanho];
+		indice = 0;
+	}
+	public void inserir(Evento evento) throws EventoExistenteException{
+		if(!this.existe(evento.getNome())){
+			this.eventos[indice] = evento;
+			this.indice++;	
+		}else {
+			throw new EventoExistenteException();
+		}
+	}
+
+	public void remover(String nome) throws EventoNaoExisteException{
+		if(this.existe(nome)){
+			this.eventos[this.indice] = this.eventos[this.indice-1];
+			this.indice = this.indice - 1;
+		}else {
+			 throw new EventoNaoExisteException();
+		}
+	}
+
+	public void atualizar(String nome,Evento nomeNovo) throws EventoNaoExisteException{
+		this.eventos[getIndice(nome)] = nomeNovo;
+	}
+
+	public Evento procurar(String nome) throws EventoNaoExisteException{
+		return this.eventos[this.getIndice(nome)];
+	
+		
+	}
+
+	public boolean existe(String nome) {
+		for(int i = 0;i <this.indice;i++){
+			if(this.eventos[i].getNome().equals(nome)){
+				return true;
+			}
+		}
+		
+		return false;
+	}
+	public int getIndice(String nome)	throws EventoNaoExisteException{
+		for(int i = 0;i < this.indice;i++){
+			if(this.eventos[i].getNome().equals(nome)){
+				return i;
+			}
+		}
+		throw new EventoNaoExisteException();
+	}
+	
+
+}
