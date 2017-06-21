de27337adf7c6606ce590a09f47b8a8b59c1a8b5
marialuizaleal <mllb@cin.ufpe.br>
2017-06-21 14:38:09 -0300

    Add files via upload

---
+++ Livraria-ProjetoIP/src/classesNegocio/Jogo.java
@@ -0,0 +1,24 @@
+package classesNegocio;
+
+public class Jogo extends Produto {
+	private String plataforma;
+	private String genero;
+	
+	public Jogo(String codigo, double preco, String nome, int estoque) {
+		super(codigo, preco, nome, estoque);
+	}
+	public String getGenero() {
+		return this.genero;
+	}
+
+	public String getPlataforma() {
+		return this.plataforma;
+	}
+	public void setGenero(String gen) {
+		this.genero = gen;
+	}
+
+	public void setPlataforma(String plataforma) {
+		this.plataforma = plataforma;
+	}
+}
